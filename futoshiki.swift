8e06f83e1b9b2b95bb3b7f68eca81b4f18883c3f
jdeeda <jldeda00@gmail.com>
2021-12-18 10:46:50 -0500

    cleaned futoshiki.swift

--- futoshiki.swift
+++ futoshiki.swift
@@ -10,61 +10,39 @@ import Foundation
 import DequeModule
 import OrderedCollections
 
-// TODO: program loops one extra step and causes failure.
-
-func printer(_ board: [[Variable]]) {
-  board.forEach { row in
-    row.forEach { item in
-      print(item.value, separator: " ", terminator: " ")
-    }
-    print("\n")
-  }
-  print("\n")
-}
-
-func printer_domains(_ board: [[Variable]]) {
-  board.forEach { row in
-    row.forEach { item in
-      print(item.domain.count, separator: " ", terminator: " ")
-    }
-    print("\n")
-  }
-  print("\n")
-}
-
 struct Pair: Hashable {
   let Xi: Variable
   let Xj: Variable
 }
 
-struct Index: Hashable { // baggage. fixed sized array? tuples are not hashable.
+struct Index: Hashable {
   let x: Int
   let y: Int
 }
 
-// MUTATES.
 struct Variable: Hashable {
   var value: Int
   var domain: OrderedSet<Int>
-  let constraints: Dictionary<Index, Bool> // messy.
+  let constraints: Dictionary<Index, Bool>
   let index: Index
-  let predetermined: Bool // oobject should really be immutable...
+  let predetermined: Bool // object should really be immutable...
 }
 
-// MUTATES.
 class Assignment {
+  let dim: Int
   var board: [[Variable]]
-  var failure: Bool
-  var unassignedIdx: Index // baggage.
-  var unassigned: Variable {
-    return self.board[self.unassignedIdx.x][self.unassignedIdx.y]
+  var firstUnassignedIndex: Index
+  var firstUnassigned: Variable {
+    return self.board[self.firstUnassignedIndex.x][self.firstUnassignedIndex.y]
   }
-  let defaultAssignment = -1
+  let unassignedValue = -1
+  let failure: Bool
   
   init (board: [[Variable]], failure: Bool) {
     self.board = board
     self.failure = failure
-    self.unassignedIdx = Index(x: 0, y:0)
+    self.firstUnassignedIndex = Index(x: 0, y:0)
+    self.dim = board.count
   }
   
   convenience init(failure: Bool) {
@@ -73,91 +51,88 @@ class Assignment {
   
   func moveIndexForward() {
     repeat {
-      if self.unassignedIdx.y + 1 == self.board.count {
-        if self.unassignedIdx.x == self.board.count - 1 { // Hacky.
-          self.unassignedIdx = Index(x: self.board.count - 1, y: self.board.count - 1)
+      if self.firstUnassignedIndex.y + 1 == self.dim {
+        if self.firstUnassignedIndex.x == self.dim - 1 { // Hacky.
+          self.firstUnassignedIndex = Index(x: self.dim - 1, y: self.dim - 1)
           return
         }
-        self.unassignedIdx = Index(x: self.unassignedIdx.x + 1, y: 0)
+        self.firstUnassignedIndex = Index(x: self.firstUnassignedIndex.x + 1, y: 0)
       }
       else {
-        self.unassignedIdx = Index(x: self.unassignedIdx.x, y: unassignedIdx.y + 1)
+        self.firstUnassignedIndex = Index(x: self.firstUnassignedIndex.x, y: firstUnassignedIndex.y + 1)
       }
-    } while (self.board[self.unassignedIdx.x][self.unassignedIdx.y].predetermined)
+    } while (self.board[self.firstUnassignedIndex.x][self.firstUnassignedIndex.y].predetermined)
   }
   
   func moveIndexBackward() {
     repeat {
-      if self.unassignedIdx.y - 1 == -1 {
-        if self.unassignedIdx.x == 0 { // Hacky.
-          self.unassignedIdx = Index(x: 0, y: 0)
+      if self.firstUnassignedIndex.y - 1 == -1 {
+        if self.firstUnassignedIndex.x == 0 {
+          self.firstUnassignedIndex = Index(x: 0, y: 0)
           return
         }
-        self.unassignedIdx = Index(x: self.unassignedIdx.x - 1, y: self.board.count - 1)
+        self.firstUnassignedIndex = Index(x: self.firstUnassignedIndex.x - 1, y: self.dim - 1)
       }
       else {
-        self.unassignedIdx = Index(x: self.unassignedIdx.x, y: unassignedIdx.y - 1)
+        self.firstUnassignedIndex = Index(x: self.firstUnassignedIndex.x, y: firstUnassignedIndex.y - 1)
       }
-    } while (self.board[self.unassignedIdx.x][self.unassignedIdx.y].predetermined)
+    } while (self.board[self.firstUnassignedIndex.x][self.firstUnassignedIndex.y].predetermined)
     
   }
   
-  // DONE.
   func assign(_ variable: Variable, _ value: Int) {
     self.board[variable.index.x][variable.index.y].value = value
     moveIndexForward()
   }
   
-  // DONE.
   func unassign(_ variable: Variable) {
-    self.board[variable.index.x][variable.index.y].value = self.defaultAssignment
+    self.board[variable.index.x][variable.index.y].value = self.unassignedValue
     moveIndexBackward()
     
   }
   
-  // DONE.
+  func isUnassigned(_ variable: Variable) -> Bool {
+    return variable.value == self.unassignedValue
+  }
+  
   func neighbors(of: Variable, except: Variable) -> [Variable] {
     let row = self.board[of.index.x]
-    let col = self.board[0..<self.board.count].map { $0[of.index.y] }
+    let col = self.board[0..<self.dim].map { $0[of.index.y] }
     var neighbors = Set(row + col)
     neighbors.remove(except)
     neighbors.remove(of)
     return Array(neighbors)
   }
   
-  func neighbors(of: Variable) -> [Variable] { // Should really be constraint domain...
+  func neighbors(of: Variable) -> [Variable] {
     let row = self.board[of.index.x]
-    let col = self.board[0..<self.board.count].map { $0[of.index.y] }
+    let col = self.board[0..<self.dim].map { $0[of.index.y] }
     var neighbors = Set(row + col)
     neighbors.remove(of)
     return Array(neighbors)
   }
 }
 
-// DONE.
 enum Filter: String {
   case MAC = "MAC"
   case NONE = "none"
 }
 
-// DONE.
 enum Order: String {
   case normal = "normal"
+  case minimal = "minimal"
 }
 
 struct CSP {
   
-  // DONE.
   func isComplete(_ assignment: Assignment) -> Bool {
-    let c = assignment.board.count - 1
-    let last = assignment.board[c][c]
-    if assignment.unassignedIdx.x == c && assignment.unassignedIdx.y == c && assignment.unassigned.value != -1 {
+    let last = assignment.board[assignment.dim - 1][assignment.dim - 1]
+    if !assignment.isUnassigned(last) {
       return isConsistent(last.value, last, assignment)
     }
     return false
   }
   
-  // DONE.
   func isConsistent(_ value: Int, _ variable: Variable, _ assignment: Assignment) -> Bool {
     
     // Copy variable and update its value for testing.
@@ -167,27 +142,27 @@ struct CSP {
     // Check for valid AB pairs and or duplicates in corresponding row.
     let row = assignment.board[test.index.x]
     for rowVar in row {
-      if rowVar.index.x == test.index.x && rowVar.index.y == test.index.y {
+      if rowVar.index == test.index {
         continue
       }
       if test.value == rowVar.value {
         return false
       }
-      if isAdjacent(rowVar.index.y, test.index.y, assignment.board.count) && rowVar.value != -1 && !isConsistentAB(rowVar, test) {
+      if isAdjacent(rowVar, test, assignment.dim) && !assignment.isUnassigned(rowVar) && !isConsistentAB(rowVar, test) {
         return false
       }
     }
     
     // Check for valid AB pairs and or duplicates in corresponding col.
-    let col = assignment.board[0..<assignment.board.count].map { $0[test.index.y] }
+    let col = assignment.board[0..<assignment.dim].map { $0[test.index.y] }
     for colVar in col {
-      if colVar.index.x == test.index.x && colVar.index.y == test.index.y {
+      if colVar.index == test.index {
         continue
       }
       if test.value == colVar.value {
         return false
       }
-      if isAdjacent(colVar.index.x, test.index.x, assignment.board.count) && colVar.value != -1 && !isConsistentAB(colVar, test) {
+      if isAdjacent(colVar, test, assignment.dim) && !assignment.isUnassigned(colVar) && !isConsistentAB(colVar, test) {
         return false
       }
     }
@@ -197,8 +172,20 @@ struct CSP {
   }
   
   // DONE.
-  func isAdjacent(_ idx1: Int, _ idx2: Int, _ n: Int) -> Bool {
-    return (idx1 == idx2 + 1 && idx2 + 1 < n) || (idx1 == idx2 - 1 && idx2 - 1 >= 0)
+  func isAdjacent(_ var1: Variable, _ var2: Variable, _ n: Int) -> Bool {
+    if var1.index.x == var2.index.x {
+      return isAdjacentIndex(var1.index.y, var2.index.y, n)
+    }
+    else if var1.index.y == var2.index.y {
+      return isAdjacentIndex(var1.index.x, var2.index.x, n)
+    }
+    else {
+      return false
+    }
+  }
+  
+  func isAdjacentIndex(_ i: Int, _ j: Int, _ n: Int) -> Bool {
+    return (i == j + 1 && j + 1 < n) || (i == j - 1 && j - 1 >= 0)
   }
   
   // DONE.
@@ -213,10 +200,10 @@ struct CSP {
     }
     
     // Test conditions.
-    if var1LessThan == true && var2LessThan == false {
+    if var1LessThan && !var2LessThan {
       return var1.value < var2.value
     }
-    else if var1LessThan == false && var2LessThan == true {
+    else if !var1LessThan && var2LessThan {
       return var1.value > var2.value
     }
     else {
@@ -224,7 +211,6 @@ struct CSP {
     }
   }
   
-  // DONE.
   func inference(_ assignment: Assignment, _ filter: Filter) -> Bool {
     switch filter {
     case .MAC: return self.inferMAC(assignment)
@@ -232,25 +218,23 @@ struct CSP {
     }
   }
   
-  // DONE.
   func revise(_ Xi: inout Variable, _ Xj: Variable, _ assignment: Assignment) -> Bool {
     var revised = false
     for x in Xi.domain {
       var noVal = true
       for y in Xj.domain{
-        if x != y { // If Xi Xj adjacent and AB pair...could be a problem.
+        if x != y { // TODO: If Xi Xj adjacent and AB pair...could be a problem.
           noVal = false
         }
       }
       if noVal {
-        assignment.board[Xi.index.x][Xi.index.y].domain.remove(x) // Cheesy?
+        assignment.board[Xi.index.x][Xi.index.y].domain.remove(x)
         revised = true
       }
     }
     return revised
   }
   
-  // TODO: Broken. Queue initialized incorrectly (pairs)?
   func inferMAC(_ assignment: Assignment) -> Bool {
     
     // Make queue.
@@ -270,7 +254,7 @@ struct CSP {
       if Xi.predetermined {
         continue
       }
-      if revise(&Xi, Xj, assignment) {
+      if revise(&Xi, Xj, assignment) { // TODO: amper?
         if Xi.domain.isEmpty {
           return false
         }
@@ -285,7 +269,7 @@ struct CSP {
 
 func backtrack(assignment: Assignment, csp: CSP, filter: Filter) -> Assignment {
   if csp.isComplete(assignment) { return assignment }
-  let variable = assignment.unassigned
+  let variable = assignment.firstUnassigned
   for value in variable.domain {
     if csp.isConsistent(value, variable, assignment) {
       assignment.assign(variable, value)
